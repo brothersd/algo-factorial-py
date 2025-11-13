07c2423e45356b327a9fe73cf3e17d7cf16e0ac1
Dan Brothers <brothersd@hotmail.com>
2025-11-12 19:48:18 -0700

    remove else statement

--- factorial.py
+++ factorial.py
@@ -2,11 +2,10 @@ def factorial(num):
 	# your code here
 	if num == 0 or num == 1:
 		return 1
-	else: 
-		return (num * factorial(num - 1))
+	return (num * factorial(num - 1))
+	
+	
 		
-
-
-# print(factorial(8) == 40320)
-# print(factorial(18) == 6402373705728000)
-# print(factorial(45) == 119622220865480194561963161495657715064383733760000000000)
+print(factorial(8) == 40320)
+print(factorial(18) == 6402373705728000)
+print(factorial(45) == 119622220865480194561963161495657715064383733760000000000)
